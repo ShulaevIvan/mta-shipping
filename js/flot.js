7be7fab144a1934e43634b5bb1777d6ef52bf35e
shulaev ivan <mail@ntdservice.ru>
2025-09-15 12:12:42 +0300

    add some fixes

--- js/flot.js
+++ js/flot.js
@@ -3,10 +3,12 @@ window.addEventListener('DOMContentLoaded', () => {
         flotItems: [
             {
                 id: 1,
-                title: 'Title 1',
-                imgMin: 'img/flot/no_image.png',
-                imgFull: 'img/flot/uran.jpeg',
+                title: 'Зубр',
+                imgMin: 'img/flot/zubr.jpeg',
+                imgFull: 'img/flot/zubr.jpeg',
+                altImg: 'буксир mta-shipping zubr',
                 popupActive: false,
+                popupBlocked: false,
                 flotStats: [
                     {
                         id: 1,
@@ -117,27 +119,98 @@ window.addEventListener('DOMContentLoaded', () => {
             },
             {
                 id: 2,
-                title: 'Title 2',
-                  imgMin: 'img/flot/no_image.png',
-                imgFull: '',
+                title: 'Уран',
+                imgMin: 'img/flot/uran.jpeg',
+                imgFull: 'img/flot/uran.jpeg',
+                altImg: 'буксир mta-shipping Уран',
                 popupActive: false,
+                popupBlocked: false,
+                flotStats: [
+                    {
+                        id: 1,
+                        name: 'регистровый №',
+                        value: '860935'
+                    },
+                    {
+                        id: 2,
+                        name: 'ИМО',
+                        value: '8727408'
+                    },
+                    {
+                        id: 3,
+                        name: 'порт приписки',
+                        value: 'Санкт-Петербург'
+                    },
+                    {
+                        id: 4,
+                        name: 'флаг',
+                        value: 'Россия'
+                    },
+                    {
+                        id: 5,
+                        name: 'символ класса РС',
+                        value: 'KM(*) UL R2 AUT2 FF3WS Tug'
+                    },
+                    {
+                        id: 6,
+                        name: 'тип и назначение',
+                        value: 'буксир'
+                    },
+                    {
+                        id: 7,
+                        name: 'валовая вместимость',
+                        value: '279 т'
+                    },
+                    {
+                        id: 8,
+                        name: 'длина наибольшая',
+                        value: '35,78 м'
+                    },
+                    {
+                        id: 9,
+                        name: 'ширина',
+                        value: '9,00 м'
+                    },
+                    {
+                        id: 10,
+                        name: 'высота борта',
+                        value: '4,50 м'
+                    },
+                    {
+                        id: 11,
+                        name: 'суммарная мощность',
+                        value: '1852 кВт (2500 л.с.)'
+                    },
+                    {
+                        id: 12,
+                        name: 'тип главного двигателя',
+                        value: 'ДВС 6ASL 25D'
+                    },
+                    {
+                        id: 13,
+                        name: 'буксирная лебедка',
+                        value: '117 kN, OHW-20, гидравлическая'
+                    },
+                    {
+                        id: 14,
+                        name: 'основной буксирный трос',
+                        value: 'Ø 38 мм., L 500 м.'
+                    },
+                ]
             },
             {
                 id: 3,
-                title: 'Title 3',
+                title: 'В разработке',
                 imgMin: 'img/flot/no_image.png',
                 imgFull: '',
+                altImg: 'В разработке',
                 popupActive: false,
-            },
-            {
-                id: 4,
-                title: 'Title 4',
-                imgMin: 'img/flot/no_image.png',
-                imgFull: '',
-                popupActive: false,
+                popupBlocked: true,
+                flotStats: []
             }
         ],
     };
+    const mainMenu = document.querySelector('.navbar');
     const flotWrap = document.querySelector('.flot-wrap');
     const popupWrap = flotWrap.querySelector('.flot-item-popup-wrap');
     const popupMainTitle = popupWrap.querySelector('.flot-item-popup-main-title');
@@ -147,7 +220,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const popupHandler = (flotItemObj, status = false) => {
         if (!flotItemObj) {
-            flotState.flotItems.map((flotItem) => {
+            flotState.flotItems = flotState.flotItems.map((flotItem) => {
                 return {
                     ...flotItem,
                     popupActive: false
@@ -155,10 +228,29 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
         if (!status) {
+            flotState.flotItems = flotState.flotItems.map((flotItem) => {
+                return {
+                    ...flotItem,
+                    popupActive: false
+                }
+            });
             popupWrap.classList.add('closed-popup');
-            popupStatsWrap.querySelectorAll('.flot-item-popup-stats-row').remove();
+            const allRows = popupStatsWrap.querySelectorAll('.flot-item-popup-stats-row');
+            allRows.forEach((rowItem) => rowItem.remove());
             return;
         }
+        flotState.flotItems = flotState.flotItems.map((flotItem) => {
+            if (flotItem.id === Number(flotItemObj.id)) {
+                return {
+                    ...flotItem,
+                    popupActive: true
+            }
+            }
+            return {
+                ...flotItem,
+                popupActive: false
+            }
+        });
         popupWriteData(flotItemObj);
         popupWrap.classList.remove('closed-popup');
     };
@@ -185,9 +277,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const showHidePopupHandler = (e) => {
         const targetItem = e.target;
         const popupId = targetItem.parentNode.parentNode.getAttribute('id');
+        mainMenu.style.zIndex = '9';
         if (!targetItem || !popupId) return;
         flotState.flotItems.map((flotItem) => {
-            if (flotItem.id === Number(popupId)) {
+            if (flotItem.id === Number(popupId) && !flotItem.popupBlocked) {
                 popupHandler(flotItem, true);
                 return {
                     ...flotItem,
@@ -227,5 +320,16 @@ window.addEventListener('DOMContentLoaded', () => {
         const flotItem = createFlotItemElement(item);
         flotWrap.appendChild(flotItem);
     });
-    closePopupBtn.addEventListener('click', () => popupHandler(false, false))
+    closePopupBtn.addEventListener('click', () => popupHandler(false, false));
+    window.document.body.addEventListener('click', (e) => {
+        const target = e.target;
+        const popupIsActive = target.classList.contains('tm-row') || 
+            target.classList.contains('tm-content') || 
+                target.classList.contains('mb-5') || 
+                    target.classList.contains('tm-site-footer');
+        if (target && popupIsActive) {
+            popupHandler(false, false);
+            mainMenu.style.zIndex = '11';
+        }
+    });
 });
